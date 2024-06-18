473f04c3b3958fdf9e048479331c6e2ed665ba5f
Deyson Flores <deyson.flores@tecsup.edu.pe>
2024-06-17 19:04:02 -0500

    DF
    XD

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,8 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in as Admin!") }}
-                    <p><a href="notifications" class="btn btn-primary">Notifications</a></p>
+                    {{ __("Iniciastes Sesion como Administrador!") }}
+                    <p><a href="notifications" class="btn btn-primary">Notificaciones</a></p>
+                </div>
+                <div class="p-4 text-gray-900 dark:text-gray-100">
+                    <p><a href="notifications" class="btn btn-primary">Vuelos</a></p>
                 </div>
             </div>
         </div>
