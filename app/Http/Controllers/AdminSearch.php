d19fb7bc877a7649850716c2fe3d0990279ff8da
RedDeadth <mimamamemima335@gmail.com>
2024-06-25 19:25:43 -0500

    sincronizar cambios

---
+++ app/Http/Controllers/AdminSearch.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Destination;
+use App\Models\Airline;
+use App\Models\Reservation;
+
+class AdminSearchController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Buscar destinos
+        $destinations = Destination::where('name', 'like', '%' . $query . '%')->get();
+
+        // Buscar aerolíneas
+        $airlines = Airline::where('name', 'like', '%' . $query . '%')->get();
+
+        // Buscar reservas
+        $reservations = Reservation::where('some_field', 'like', '%' . $query . '%')->get();
+
+        return view('admin_search_results', compact('destinations', 'airlines', 'reservations', 'query'));
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,6 +2,9 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
+
+            <!-- AQUI IRA LA BARRA DE BUSQUEDA -->
+            
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
@@ -9,6 +12,8 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
+
+                
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
