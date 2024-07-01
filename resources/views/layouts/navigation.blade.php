40d969d0813a0bd20cf1fe58d39f3be420107287
alberth <alberthrolando158@gmail.com>
2024-07-01 16:29:38 -0500

    estilos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,98 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <header class="bg-blue-500 bg-opacity-50 fixed w-full z-10">
+        <div class="container mx-auto flex justify-between items-center px-4 py-4">
+            <!-- Logo y nombre -->
+            <div class="flex justify-between items-center w-full lg:w-auto">
+                <a href="{{ route('dashboard') }}" class="flex items-center mx-auto lg:mx-0">
+                    <span class="sr-only">Inicio</span>
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />  
+                    <span class="text-white text-lg font-semibold ml-2 hidden lg:block">RESERVASYA</span>
+                </a>
+
+                <div class="flex items-center">
+                    <form action="{{ Auth::user()->isAdmin() ? route('admin.search.index') : route('search.index') }}" method="GET">
+                        <input type="text" name="search" placeholder="Buscar..." class="px-2 py-1 border border-gray-300 rounded-md">
+                        <button type="submit" class="ml-2 px-4 py-1 bg-blue-500 text-white rounded-md">Buscar</button>
+                    </form>
+                </div>
+
+                <!-- Botón de menú para dispositivos móviles -->
+                <div class="lg:hidden ml-4">
+                    <button id="mobile-menu-button" class="text-white focus:outline-none">
+                        <svg id="menu-icon" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                        <svg id="close-icon" class="w-6 h-6 hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+            <!-- Menú de navegación -->
+            <nav class="hidden lg:flex items-center space-x-4">
+                <ul class="flex space-x-4">
+                    <li><a href="#" class="text-white hover:text-gray-200">Reservas</a></li>
+                    <li><a href="#" class="text-white hover:text-gray-200">Metodos de Pago</a></li>
+                </ul>
+            </nav>
+
+            <!-- Menú de usuario -->
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::user()->name }}</div>
+
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
+        </div>
+
+        <!-- Menú desplegable para dispositivos móviles -->
+        <div id="mobile-menu" class="lg:hidden fixed inset-0 bg-blue-500 bg-opacity-90 hidden z-50">
+            <div class="flex justify-end p-4">
+                <button id="mobile-menu-close-button" class="text-white focus:outline-none">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+            </div>
+            <div class="flex justify-start">
+                <ul class="text-white text-sm text-left py-2 space-y-2 w-full">
+                    <li><a href="#" class="block py-2 px-4 hover:bg-blue-600">Reservas</a></li>
+                    <li><a href="#" class="block py-2 px-4 hover:bg-blue-600">Metodos de Pago</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </header>
+    <script src="{{ asset('js/scripts.js') }}"></script>
+
+
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
