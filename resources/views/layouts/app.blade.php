63a58f5f361884c1a384f3adbf9fa9844b1285fb
Deyson Flores <deyson.flores@tecsup.edu.pe>
2024-07-01 21:27:00 -0500

    LAYOUT
    Y boton  y navigation.php, bore lo dupllicad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,14 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="py-10">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
     </body>
